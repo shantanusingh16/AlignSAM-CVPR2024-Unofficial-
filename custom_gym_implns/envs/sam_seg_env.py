Fixed bug in initial probabilities for sam_pred_mask. Added temperature scaling based on number of steps in explicit agent for SAM proability mask. Helps prevent harsh suppression of SAM feature embeddings early in the episode.

custom_gym_implns/envs/sam_seg_env.py
@@ -71,6 +71,7 @@ class SamSegEnv(gym.Env):
                 "target_category": spaces.Text(max_length=max(map(len, target_categories))),
                 "sam_image_embeddings": spaces.Box(-np.inf, np.inf, shape=(*embedding_shape,), dtype=np.float32),
                 "sam_pred_mask_prob": spaces.Box(0, 1, shape=(*mask_shape,), dtype=np.float32),
+                "num_steps": spaces.Discrete(max_steps + 1 if max_steps is not None else 1000),
             }
         )
 
@@ -126,7 +127,7 @@ class SamSegEnv(gym.Env):
         self._image = cv2.resize(img, self.img_shape[:2][::-1])
         self.sam_predictor.set_image(self._image)
         self._sam_image_embeddings = self.sam_predictor.get_image_embeddings()
-        self._sam_pred_mask_prob = np.zeros(self.mask_shape, dtype=np.float32)
+        self._sam_pred_mask_prob = np.ones(self.mask_shape, dtype=np.float32) * 0.5
         self._sam_pred_mask = np.zeros(self.img_shape[:2], dtype=np.float32)
 
         resized_mask = cv2.resize(categorical_instance_masks, 
@@ -145,6 +146,7 @@ class SamSegEnv(gym.Env):
             "target_category": self._curr_target_cat,
             "sam_image_embeddings": self._sam_image_embeddings,
             "sam_pred_mask_prob": self._sam_pred_mask_prob,
+            "num_steps": self._num_steps,
         }
     
 
@@ -197,6 +199,7 @@ class SamSegEnv(gym.Env):
             dice_gain = max(0, dice_score - self._last_best_score)
             self._last_best_score = max(dice_score, self._last_best_score)
         else:
+            dice_score = 0.0
             dice_gain = 0.0
 
         correct_input_reward = 0
@@ -259,9 +262,8 @@ class SamSegEnv(gym.Env):
         input_point, input_type = self._action_to_input[action]
         
         terminated = input_type == 'done'
-        trunc = (self.max_steps is not None and self._num_steps >= self.max_steps)
-        if terminated or trunc:
-            return self._get_obs(), 0, terminated, trunc, self._get_info()
+        if terminated:
+            return self._get_obs(), 0, terminated, False, self._get_info()
         
     
         act = None
@@ -290,7 +292,7 @@ class SamSegEnv(gym.Env):
             self.run_sam()
         else:
             # Set the mask and mask_prob to initial state
-            self._sam_pred_mask_prob = np.zeros(self.mask_shape, dtype=np.float32)
+            self._sam_pred_mask_prob = np.ones(self.mask_shape, dtype=np.float32) * 0.5
             self._sam_pred_mask = np.zeros(self.img_shape[:2], dtype=np.float32)
         
         self._num_steps += 1
@@ -299,6 +301,8 @@ class SamSegEnv(gym.Env):
 
         observation = self._get_obs()
         info = self._get_info()
+
+        trunc = (self.max_steps is not None and self._num_steps >= self.max_steps)
 
         return observation, reward, False, trunc, info
     

models/explicit_agent.py
@@ -76,6 +76,14 @@ class ExplicitAgent(nn.Module):
         self.actor = layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
         self.critic = layer_init(nn.Linear(512, 1), std=1)
 
+        def get_base_env(env):
+            while hasattr(env, 'env'):
+                env = env.env
+            return env
+        
+        base_env = get_base_env(envs.envs[0])
+        self.max_steps = base_env.max_steps if hasattr(base_env, 'max_steps') else None
+
 
     def setup_clip(self, clip_model_name, clip_image_size, clip_text_prompt):
         clip_model, _ = clip.load(clip_model_name, device='cpu')
@@ -169,8 +177,16 @@ class ExplicitAgent(nn.Module):
         resized_sam_mask_prob = nn.functional.interpolate(
             sam_pred_mask_prob, size=(embedding_shape[2], embedding_shape[3]), 
             mode="bilinear", align_corners=False)
-        resized_sam_mask_prob = resized_sam_mask_prob.repeat(1, embedding_shape[1], 1, 1)
 
+        # Rescale the mask probabilities based on number of steps
+        if ("num_steps" in obs) and self.max_steps is not None:
+            eps = 1e-6
+            temp = torch.exp(self.max_steps - obs["num_steps"])  # Exponential decay
+            p = torch.clamp(resized_sam_mask_prob, min=eps, max=1-eps)
+            logit = torch.logit(p)
+            resized_sam_mask_prob = torch.sigmoid(logit / temp.view(-1, 1, 1, 1))
+
+        resized_sam_mask_prob = resized_sam_mask_prob.repeat(1, embedding_shape[1], 1, 1)
         x = sam_image_embeddings * resized_sam_mask_prob 
         x += sam_image_embeddings # skip connection
 

train_sam_align_ppo.py
@@ -224,7 +224,11 @@ if __name__ == "__main__":
             if isinstance(space, gym.spaces.Box):
                 obs[key] = torch.zeros((args.num_steps, args.num_envs) + space.shape).to(device)
             elif isinstance(space, gym.spaces.Text):
-                obs[key] = np.array([""] * (args.num_steps * args.num_envs), dtype=object).reshape((args.num_steps, args.num_envs))
+                obs[key] = np.array([""] * (args.num_steps * args.num_envs), 
+                                    dtype=object).reshape((args.num_steps, args.num_envs))
+            elif isinstance(space, gym.spaces.Discrete):
+                obs[key] = torch.zeros((args.num_steps, args.num_envs), 
+                                       dtype=torch.int64).to(device)
             else:
                 raise NotImplementedError(f"Unsupported observation space type: {type(space)}")
         actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)